e8c0dd4e3f8fe67e0a43e1432981efea2cb0a4c0
AdrianGomezLuna <adrigoluna@gmail.com>
2021-11-25 17:55:22 +0100

    card

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FormReactiComponent } from './components/formReacti/form-reacti/form-reacti.component';
+import { CardComponent } from './components/card/card.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     FormReactiComponent,
+    CardComponent,
 
   ],
   imports: [
